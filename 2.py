daba7ba22322c12cc92444ee408748e6a333ec4d
IrinchinovZh <exp.bur@gmail.com>
2023-02-10 11:30:58 +0800

    change

--- 2.py
+++ 2.py
@@ -3,3 +3,12 @@
 
 # 2 2
 # 4
+
+def sum_2(a, b):
+    return (sum_2(a + 1, b - 1) if b else a)
+
+
+a = int(input("Vvedite 1 chislo: "))
+b = int(input("Vvdedite 2 chislo: "))
+
+print("Resultat summi chisel", a, "i", b, "ravna", sum_2(a, b))
